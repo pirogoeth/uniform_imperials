79abdc289f754804a02a5e7aa03de749227b0f33
Sean Johnson <sean.johnson@maio.me>
2016-03-29 00:57:12 -0500

    Restructure base app to include a nav drawer that utilizes fragments.

---
+++ app/herald/src/main/java/com/uniform_imperials/herald/BaseActivity.java
@@ -0,0 +1,261 @@
+package com.uniform_imperials.herald;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uniform_imperials.herald.activities.MainActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Sean Johnson on 3/28/2016.
+ *
+ * This class is the base activity that all other application activities should extend.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    /**
+     * Global drawer layout.
+     */
+    protected DrawerLayout mDrawer;
+
+    /**
+     * Global navigation view.
+     */
+    protected NavigationView mNavView;
+
+    /**
+     * Toolbar replacing the ActionBar.
+     */
+    protected Toolbar mToolbar;
+
+    /**
+     * Linked state for the nav icon animation.
+     */
+    protected ActionBarDrawerToggle mDrawerToggle;
+
+    /**
+     * This method is the entry point for a given activity. In this method,
+     * we should load up any necessary components that may be used by the subclassing
+     * activity, such as the database connection, HTTP connection, etc.
+     *
+     * @param savedInstanceState Activity's saved instance state.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Wrapper for setContentView that binds Butterknife after setting the current view.
+     *
+     * @param resId Resource ID to use as view.
+     */
+    @Override
+    public void setContentView(@LayoutRes int resId) {
+        super.setContentView(resId);
+
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Wrapper for setContentView that binds Butterknife after setting the current view.
+     *
+     * @param view View to assign.
+     */
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Wrapper for setContentView that binds Butterknife after setting the current view.
+     *
+     * @param view View to assign
+     * @param params Parameters for the view
+     */
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
+
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Finds the views and resources associated with creating an inter-Activity navigation
+     * drawer.
+     */
+    protected void createNavDrawer() {
+        if (this.mToolbar == null) {
+            this.mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+
+        if (this.mDrawer == null) {
+            this.mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+
+        if (this.mNavView == null) {
+            this.mNavView = (NavigationView) findViewById(R.id.nav_view);
+        }
+
+        // Override the navigation icon.
+        this.mToolbar.setNavigationIcon(R.drawable.nav_drop);
+
+        // Override the actionbar with a new MD toolbar.
+        setSupportActionBar(this.mToolbar);
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (Exception e) {
+            System.out.println("Something went horribly wrong...");
+            e.printStackTrace();
+        }
+
+        // Set up the linked mDrawer + mToolbar toggle
+        mDrawerToggle = setupDrawerToggle();
+
+        // Run the routine to build nav drawer content.
+        setupDrawerContent(this.mNavView);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(
+                this,
+                this.mDrawer,
+                this.mToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+    }
+
+    /**
+     * Event for opening the nav drawer.
+     *
+     * @param item item that was selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (this.mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Post-create routine.
+     *
+     * @param savedInstanceState Activity's saved instance state.
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        if (this.mDrawerToggle != null) {
+            this.mDrawerToggle.syncState();
+        }
+    }
+
+    /**
+     * Event listener for Activity configuration change (ie., screen rotation)
+     *
+     * @param config new Activity config
+     */
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+
+        // Pass config changes to the drawer toggle.
+        if (this.mDrawerToggle != null) {
+            this.mDrawerToggle.onConfigurationChanged(config);
+        }
+    }
+
+    /**
+     * Creates a nav selection listener to select a new drawer item.
+     *
+     * @param nav active navigation menu.
+     */
+    private void setupDrawerContent(NavigationView nav) {
+        nav.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        selectDrawerItem(item);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    /**
+     * Sets the current displayed fragment based on a clicked nav menu item.
+     *
+     * @param item Menu item that was selected.
+     */
+    public void selectDrawerItem(MenuItem item) {
+        // Create a fragment to show the activity specified by the nav item.
+        Fragment mFrag = null;
+        Class cFrag = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_am:  // Application manager pane
+                // cFrag = ApplicationManagerActivity.class;
+                break;
+            case R.id.nav_dm:  // Device manager pane
+                // cFrag = DeviceManagerActivity.class;
+                break;
+            case R.id.nav_nh:  // Notification history pane1
+                // cFrag = NotificationHistoryActivity.class;
+                break;
+            case R.id.nav_sp:  // Settings pane
+                // cFrag = SettingsActivity.class;
+                break;
+            case R.id.nav_ap:  // About pane
+                // cFrag = AboutPanelActivity.class
+                break;
+            default:
+                cFrag = MainActivity.class;
+                break;
+        }
+
+        try {
+            mFrag = (Fragment) cFrag.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Replace the current Fragment.
+        FragmentManager mFM = getSupportFragmentManager();
+        mFM.beginTransaction()
+           .replace(R.id.content_frame, mFrag)
+           .commit();
+
+        // Highlight the current fragment in the nav drawer.
+        item.setChecked(true);
+
+        // Set toolbar title.
+        setTitle(item.getTitle());
+
+        // Close the nav!
+        this.mDrawer.closeDrawer(GravityCompat.START);
+    }
+}
---
+++ app/herald/src/main/java/com/uniform_imperials/herald/activities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.uniform_imperials.herald.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+
+import com.uniform_imperials.herald.BaseActivity;
+import com.uniform_imperials.herald.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.global_drawer);
+
+        NavigationView nav = (NavigationView) findViewById(R.id.nav_view);
+        if (nav == null) {
+            System.out.println("Main Activity || nav is null :(");
+        } else {
+            System.out.println("Main Activity || nav WAS NOT NULL!");
+        }
+
+        this.createNavDrawer();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // this.mDrawer.openDrawer(GravityCompat.START);
+    }
+}
