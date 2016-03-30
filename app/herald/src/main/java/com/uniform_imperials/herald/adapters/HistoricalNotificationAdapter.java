997b6330b8f0ebbd73d55677b7e690a891c7942d
Sean Johnson <pirogoeth@maio.me>
2016-03-29 19:58:34 -0500

    Base implementation for notification history!

---
+++ app/herald/src/main/java/com/uniform_imperials/herald/adapters/HistoricalNotificationAdapter.java
@@ -0,0 +1,79 @@
+package com.uniform_imperials.herald.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uniform_imperials.herald.R;
+import com.uniform_imperials.herald.models.HistoricalNotification;
+
+/**
+ * Created by Sean Johnson on 3/29/2016.
+ */
+public class HistoricalNotificationAdapter extends RecyclerView.Adapter<HistoricalNotificationAdapter.ViewHolder> {
+
+    /**
+     * Dataset to display
+     */
+    private HistoricalNotification[] mDataSet;
+
+    /**
+     * Encapsulates all views needed to display a single dataset item
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // TODO: Figure out how items will be displayed in the view.
+
+        public ViewHolder(View v) {
+            super(v);
+        }
+    }
+
+    public HistoricalNotificationAdapter(HistoricalNotification[] notifs) {
+        this.mDataSet = notifs;
+    }
+
+    /**
+     * Creates a viewholder with the view for this recycler view.
+     *
+     * @param parent ViewGroup parent
+     * @param viewType view type
+     * @return new ViewHolder
+     */
+    @Override
+    public HistoricalNotificationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                       int viewType) {
+        // Create the view
+        View v = LayoutInflater.from(parent.getContext())
+                               .inflate(R.layout.anh_notification_view, parent, false);
+
+        // TODO: Set the necessary data in the view.
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    /**
+     * Binds a single dataset record to a view.
+     *
+     * @param holder viewholder element
+     * @param position dataset position
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // Get dataset element and replace view contents
+        // -- holder.whateverview.setText(this.mDataSet[position]) ...
+        return;
+    }
+
+    /**
+     * Returns dataset length
+     *
+     * @return dataset length
+     */
+    @Override
+    public int getItemCount() {
+        return this.mDataSet.length;
+    }
+
+}
---
+++ app/herald/src/main/java/com/uniform_imperials/herald/models/HistoricalNotification.java
@@ -0,0 +1,7 @@
+package com.uniform_imperials.herald.models;
+
+/**
+ * Created by pirogoeth on 3/29/2016.
+ */
+public class HistoricalNotification {
+}
