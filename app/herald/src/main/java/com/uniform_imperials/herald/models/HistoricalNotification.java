4f69109e4df24c01f947d1e364ccdd928a466cd3
Sean Johnson <pirogoeth@maio.me>
2016-04-07 14:32:56 -0600

    Update HistoricalNotification.java

--- app/herald/src/main/java/com/uniform_imperials/herald/models/HistoricalNotification.java
+++ app/herald/src/main/java/com/uniform_imperials/herald/models/HistoricalNotification.java
@@ -1,7 +1,9 @@
 package com.uniform_imperials.herald.models;
 
 /**
- * Created by pirogoeth on 3/29/2016.
+ * Created by Sean Johnson on 3/29/2016.
+ * 
+ * TODO: Extend the Requery base model and add attributes required by this model.
  */
 public class HistoricalNotification {
 }
