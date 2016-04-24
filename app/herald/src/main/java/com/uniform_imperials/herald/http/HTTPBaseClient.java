97be057fade70854b0e6ea6c2b5c70c76de101bd
Sean Johnson <pirogoeth@maio.me>
2016-04-23 23:30:22 -0500

    Restructure HTTPBaseClient and add stubbed out HttpResponse class for futures.

--- app/herald/src/main/java/com/uniform_imperials/herald/http/HTTPBaseClient.java
+++ app/herald/src/main/java/com/uniform_imperials/herald/http/HTTPBaseClient.java
@@ -11,13 +11,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
 
 
 /**
- * Created by Matt Humphrieson 4/22/2016.
+ * Created by Matt Humphries 4/22/2016.
  */
 
 /**
@@ -30,139 +35,192 @@ import java.util.HashMap;
  */
 abstract class HTTPBaseClient {
 
-	/**
-	 * Connection URL
-	 */
-	private String url;
+    /**
+     * Base API Url for requests.
+     */
+	private static URL baseApiUrl;
 
-	/**
-	 * String -> String mapping of arguments to
-	 */
-	private HashMap<String, String> args;
+    /**
+     * Returns the string representing the base API url for requests.
+     * @return
+     */
+    public static String getBaseApiUrl() {
+        if (baseApiUrl == null) {
+            return null;
+        }
 
-	public HTTPBaseClient(String url, HashMap<String, String> args){
-	}
+        return baseApiUrl.toString();
+    }
 
-	/**
-	 * Instances must be configured with setDoOutput(true) to
-	 * include a request body. GET method by default.
-	 * Transmit data by writing
-	 * to the stream returned by getOutputStream().
-	 */
-	private get(String URL, HashMap args) {
-		HttpURLConnection hc;
-		hc.setDoOutput(true);
-		URL server;
-		InputStream in;
-		OutputStream out;
-		try {
-			server = new URL(url);
-			hc = (HttpURLConnection) server.openConnection();
-			hc.setChunkedStreamingMode(0);
+    /**
+     * Sets the base API url for requests.
+     * @throws MalformedURLException if the URL given is invalid.
+     */
+    public static void setBaseApiUrl(String s) throws MalformedURLException {
+        URL apiBase = null;
 
-			writeStream(out);
-			readStream(in);
+        try {
+            apiBase = new URL(s);
+        } catch (MalformedURLException exc) {
+            throw exc;
+        } catch (Exception exc) {
+            Sentry.captureException(exc);
+        }
 
-		} catch (Exception e) {
-			Sentry.captureException(e);
-		} finally {
-			hc.disconnect();
-		}
+        baseApiUrl = apiBase;
+    }
 
-	}
+    public Future<IHttpResponse> get(String uri, HashMap<String, String> args) {
+        URL requestUrl;
 
-	//UE
-	private void put(String URL, HashMap args){
-		HttpURLConnection hc;
-		hc.setDoOutput(true);
-		URL server;
-		InputStream in;
-		OutputStream out;
-		try {
-			hc = (HttpURLConnection) server.openConnection();
-			hc.setRequestMethod(PUT);
-			hc.setChunkedStreamingMode(0);
+        try {
+            requestUrl = this.buildRequestUrl(uri, this.encodeParamString(args));
+        } catch (Exception exc) {
+            // Shit
+            return null;
+        }
 
-			writeStream(out);
-			readStream(in);
-			finally {
-				hc.disconnect();
-			}
-		}catch (Exception e) {
-			Sentry.captureException(e);
-		}
-	}
+        return null;
+    }
 
-	/**
-	 * The response body may be read from the stream
-	 * returned by getInputStream(). If the response has
-	 * no body, that method returns an empty stream.
-	 */
-	private void post(String URL, HashMap args){
-		HttpURLConnection hc;
-		hc.setDoOutput(true);
-		URL server;
-		InputStream in;
-		OutputStream out;
-		try {
-			hc = (HttpURLConnection) server.openConnection();
-			hc.setChunkedStreamingMode(0);
-			writeStream(out);
-			readStream(in);
-			finally {
-				hc.disconnect();
-			}
-		}catch (Exception e) {
-			Sentry.captureException(e);
-		}
-	}
+    public Future<IHttpResponse> post(String uri, HashMap<String, String> args) {
+        URL requestUrl;
+        String requestBody = this.encodeParamString(args);
 
-	//QS
-	private void delete(URL URL, HashMap args){
-		HttpURLConnection hc;
-		URL server;
-		OutputStream out;
-		try {
-			hc = (HttpURLConnection) server.openConnection();
-			hc.setRequestMethod(DELETE);
-			writeStream(out);
-			finally {
-				hc.disconnect();
-			}
-		} catch (Exception e) {
-			Sentry.captureException(e);
-		}
-	}
+        try {
+            requestUrl = this.buildRequestUrl(uri);
+        } catch (Exception exc) {
+            // Shit
+            return null;
+        }
 
-	private void head(String URL, HashMap args) {
-		HttpURLConnection hc;
-		hc.setDoOutput(true);
-		URL server;
-		InputStream in;
-		OutputStream out;
-		try {
-			hc = (HttpURLConnection) server.openConnection();
-			hc.setRequestMethod(HEAD);
-			readStream(in);
-		} catch (Exception e) {
-			Sentry.captureException(e);
-		}
-	}
-	private void readStream(InputStream streamIn){
-		BufferedInputStream in = new BufferedReader(streamIn);
-		String inputLn;
-		StringBuffer response = new StringBuffer();
-		try{
-			for(inputLn = in.readLine();inputLn != null; inputLn = in.readLine()){
-				response.append(inputLn);
-			}
-			in.close();
-		}catch (Exception e) {
-			Sentry.captureException(e);
-		}
+        return null;
+    }
 
-		System.out.println(response.toString());
-	}
+    public Future<IHttpResponse> put(String uri, HashMap<String, String> args) {
+        return null;
+    }
+
+    public Future<IHttpResponse> delete(String uri, HashMap<String, String> args) {
+        return null;
+    }
+
+    public Future<IHttpResponse> head(String uri, HashMap<String, String> args) {
+        return null;
+    }
+
+    private URL buildRequestUrl(String uri) throws Exception {
+        return this.buildRequestUrl(uri, null);
+    }
+
+    private URL buildRequestUrl(String uri, String params) throws Exception {
+        if (uri == null) {
+            return null;
+        }
+
+        StringBuilder requestUrl = new StringBuilder();
+
+        // First, process the base url.
+        String baseUrl = baseApiUrl.toString();
+        if (baseUrl == null) {
+            throw new Exception("Must provide a base URL before making a request.");
+        }
+
+        if (baseUrl.charAt(baseUrl.length() - 1) == '/') {
+            // Trim off the trailing slash
+            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
+        }
+
+        // Push the base url in the first position of the request url.
+        requestUrl.append(baseUrl);
+
+        if (uri.charAt(0) != '/') {
+            uri = "/" + uri;
+        }
+
+        // Push the request uri on top of the base url
+        requestUrl.append(uri);
+
+        if (params != null) {
+            // Make sure the params string begins with a '?', if present.
+            if (params.charAt(0) != '?') {
+                params = "?" + params;
+            }
+
+            // Make sure the params string does not end with &
+            if (params.charAt(params.length() - 1) == '&') {
+                params = params.substring(0, params.length() - 1);
+            }
+        }
+
+        URL fullRequestUrl;
+        try {
+            fullRequestUrl = new URL(requestUrl.toString());
+        } catch (MalformedURLException exc) {
+            // What alternatives do we have?
+            Sentry.captureException(exc);
+            throw exc;
+        } catch (Exception exc) {
+            // No other exceptions should be thrown, but be careful.
+            Sentry.captureException(exc);
+            Sentry.captureMessage("Last exception captured was unexpected!");
+
+            return null;
+        }
+
+        return fullRequestUrl;
+    }
+
+    private String encodeParamString(HashMap<String, String> params) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("?");
+
+        for (Map.Entry<String, String> ent : params.entrySet()) {
+            try {
+                sb.append(
+                        String.format(
+                                "%s=%s&",
+                                URLEncoder.encode(ent.getKey(), "UTF-8"),
+                                URLEncoder.encode(ent.getValue(), "UTF-8")
+                        )
+                );
+            } catch (UnsupportedEncodingException exc) {
+                // Figure out some way to handle this.
+                Sentry.captureException(exc);
+            } catch (Exception exc) {
+                // This should not issue any other exceptions, but be cautious
+                Sentry.captureException(exc);
+                Sentry.captureMessage("Last exception captured was unexpected!");
+
+                return null;
+            }
+        }
+
+        String paramString = sb.toString();
+        if (paramString.charAt(paramString.length() - 1) == '&') {
+            // substring is inclusive -> exclusive
+            paramString = paramString.substring(0, paramString.length() - 1);
+        }
+
+        return paramString;
+    }
+
+	private void readStream(InputStream streamIn) {
+//        BufferedInputStream in = new BufferedReader(streamIn);
+//        String inputLn;
+//        StringBuffer response = new StringBuffer();
+//        try {
+//            for (inputLn = in.readLine(); inputLn != null; inputLn = in.readLine()) {
+//                response.append(inputLn);
+//            }
+//            in.close();
+//        } catch (Exception e) {
+//            Sentry.captureException(e);
+//        }
+//
+//        System.out.println(response.toString());
+    }
+
 	private void writeStream(OutputStream write){
 		BufferedOutputStream out;
 		try {
---
+++ app/herald/src/main/java/com/uniform_imperials/herald/http/IHttpResponse.java
@@ -0,0 +1,7 @@
+package com.uniform_imperials.herald.http;
+
+/**
+ * Created by Sean on 4/23/2016.
+ */
+public interface IHttpResponse {
+}
